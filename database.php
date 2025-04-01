01969b6c306feff3116bc5fc8597b0aa9e555d6d
thamnguyen2412 <tham12apt@gmail.com>
2025-04-01 22:34:53 +0700

    Up file lần đầu

---
+++ database.php
@@ -0,0 +1,32 @@
+<?php
+class Database {
+    private $host = "localhost"; // Địa chỉ máy chủ
+    private $db_name = "qlbanhang"; // Tên cơ sở dữ liệu
+    private $username = "root"; // Tên người dùng
+    private $password = ""; // Mật khẩu
+    private $conn; // Biến lưu trữ kết nối
+
+    // Phương thức kết nối
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Báo lỗi dưới dạng Exception
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Lấy dữ liệu dưới dạng mảng kết hợp
+                PDO::ATTR_EMULATE_PREPARES => false, // Tắt giả lập prepared statements
+            ];
+            $this->conn = new PDO($dsn, $this->username, $this->password, $options);
+        } catch (PDOException $e) {
+            die("Kết nối thất bại: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+    public function close(){
+        echo 'Đóng kết nối.';
+        $this->conn=null;
+    }
+}
+?>
